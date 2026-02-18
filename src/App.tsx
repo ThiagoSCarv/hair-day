0fd7cd2ac3499750bfdbbfcc33ceb2918c276350
Thiago <thiagocarv4@gmail.com>
2026-02-18 16:09:14 -0300

    feat: componente de texto concluido

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import Text from "./components/text";
 function App() {
-	return <div className="text-blue-500">Olá Mundo!</div>;
+	return (
+		<div className="flex flex-col gap-3">
+			<Text className="text-gray-900" variant="title-lg">
+				Olá Mundo
+			</Text>
+			<Text className="text-gray-500" variant="title-sm">
+				Olá Mundo
+			</Text>
+		</div>
+	);
 }
 
 export default App;
